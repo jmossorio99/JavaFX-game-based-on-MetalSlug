f54f0e8cb027645eddd5ad9b3f6b92f3908fcb69
Camilo <camilo4182@gmail.com>
2018-11-26 18:27:10 -0500

    Documentacion de la clase TimeList y Score.

--- Metal/src/view/Main.java
+++ Metal/src/view/Main.java
@@ -1,28 +1,34 @@
 package view;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.Game;
 import threads.MusicThread;
+
+
+// ================================================================================================
+// ||                                         METAL SLUG                                         ||
+// ||                                    PROYECTO FINAL APII                                     ||
+// ================================================================================================
+
+
+/**
+ * Clase principal desde donde empieza la ejecución del programa.
+ * @author Jose Manuel Ossorio
+ * @author Juan David Aguirre
+ * @author Johan Camilo Cortés
+ * Version 1.0 - 27/11/2018
+ */
+
 
 public class Main extends Application {
 
-	private static Game game;
-
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("/view/MainWindow.fxml"));
 		Parent root = loader.load();
@@ -30,21 +36,16 @@ public class Main extends Application {
 		primaryStage.setScene(mainScene);
 		primaryStage.setTitle("Metal Slug");
 		primaryStage.show();
-
 	}
 
 	@Override
 	public void stop() {
-
 		System.exit(0);
-
 	}
 
 	public static void main(String[] args) {
-
 		MusicThread musicThread = new MusicThread();
 		musicThread.start();
 		launch();
-
 	}
 }
